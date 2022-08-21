05bcaf142524cbcc2a711ab745a309973039f0c0
krisboz <kristijan.bozinovic1@gmail.com>
2022-08-21 16:30:18 +0200

    Refactored the code, added result evaluation and reset functionality, deleted unnecessary files

--- index.js
+++ index.js
@@ -1,55 +1,49 @@
-const Gameboard = (() => {
-    //
-    let gameboard = [] ;
+//Gameboard
+const Game = (()=>{
 
-    const resetGameboard = (gb) => {
-        //for some reason {gb=[]} doesn't work
-        gb.splice(0, gb.length)
+    let board = [null, null, null, null, null, null, null, null, null];
+
+    function add(value, index) {
+        this.board[index] = value
     }
 
-    const add = (el) => {
-        gameboard.push(el)
+    function reset() {
+    this.board = [null, null, null, null, null, null, null, null, null]
     }
-    return {
-       gameboard, resetGameboard, add
-    }
+
+
+
+    return {board, add, reset}
+
+
 })();
-
-
-
+//Player
 const Player = (name, type) => {
-    //is the player with x or o
-    let position = null;
-    const play = (pos) => {
-        position = pos;
-        console.log(Gameboard.gameboard)
-        return {type, position};
+
+    const play = () => {
+
     }
-    //wins
-    //losses
-    return {name, type, play, position}
+
+    return {name, type, play}
 }
-const displayController = (() => {
-    //Update the DOM on click
-    const appendHTML = (element) => {
-        const o = ( `<span class="material-symbols-outlined" id="kruzic"> radio_button_unchecked  </span>`);
+//displayController
+const View =(()=>{
+
+    const appendHTML = (el) => {
+        const o = ( `<span class="material-symbols-outlined kruzic" id=""> radio_button_unchecked  </span>`);
         const x = (`<span class="material-symbols-outlined"> close </span>`) ;     
-        if (element == "x") {
-            return x
-        } else if (element == "o") {
-            return o
-        } else {
-            return ""
+
+        if (el === "x") {
+            return x;
+        } else if (el === "o") {
+            return o;
         }
     }
 
-   //on illegal move
-    //on win/lose
-    const updateBoard = (gameboard) => {
-        gameboard.forEach((el, index) => {
-            document.getElementById(`${el.position}`).insertAdjacentHTML("beforeend", appendHTML(el.type))
-            
-        });
+
+
+    const update = (location) => {
+        document.getElementById(location).insertAdjacentHTML("afterbegin", appendHTML(Game.board[location]))
     }
 
     const reset = () => {
@@ -60,66 +54,123 @@ const displayController = (() => {
         }
     }
 
-    return {updateBoard, reset};
+    return {update, reset}
+
 })();
+//Gameflow
 
+const Controller =(()=>{
+    let playerOne = Player("PlayerOne", "x")
+    let playerTwo = Player("PlayerTwo", "o")
+    let state = "playing";
 
-const Gameflow = (() => {
+    let turn = playerOne;
 
-    const o = Player("Neznam", "o")
-    const x = Player("Bupubu", "x");
-    let turn = x;
-
-    const checkState = () => {
-        if (Gameboard.gameboard.length >= 9) {
-            Gameboard.resetGameboard(Gameboard.gameboard);
-            displayController.reset();
-            setTimeout(()=>{
-                window.alert("It's a draw")
-            }, 3000)
-            //window.alert("It's a draw")
-
-        }
+    const resetAll = () => {
+        Game.reset(Game.board);
+        View.reset();
+        turn = playerOne;
+        state = "playing"
     }
-
 
     const changeTurn = () => {
-        if(turn == o) {
-            turn = x;
-        }else if (turn == x) {
-            turn = o;
+        if (turn === playerOne) {
+            turn = playerTwo
+        } else {
+            turn = playerOne
         }
     }
 
-    const checkLegal = (array, position) => {
-        //we get your position
-        console.log(array, position)
+    const evaluateState = () => {
+        if (state === "playing") {
+            console.log("playing");
+        } else if (state === "win") {
+            console.log("win")
+        }else if(state==="draw") {
+            console.log("draw")
+        }
+    }
 
-        array.forEach(element => {
-            if(element.position == position) {
-                console.log("Illegal")
-            } else {
-                console.log("Legal")
+    const checkResult = (e) => {
+        let winningCombos = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6]
+        ]
+
+        let boardExcerpt = []
+        let dividedBoard = []
+
+        /** 
+        Takes each combo with the current click, takes symbol value in
+        Game.board and divides that into arrays with 3 children
+        */
+        winningCombos.forEach(element => {
+            if (element.includes(parseInt(e.target.id))) {
+                element.forEach(element => {
+                    boardExcerpt.push(Game.board[element])
+                });
             }
         });
 
+        for (var i=0; i<boardExcerpt.length; i+=3) {
+            dividedBoard.push(boardExcerpt.slice(i, i+3))
+        }
+
+        //Checks if every element in Array is the same
+        dividedBoard.forEach(element => {
+            if (element.every( (val, i, arr) => val === arr[0])) {
+               state = "win"
+            }
+        });
+
+        if ((Game.board.filter(element => element===null)).length == 0) {
+            if(state === "playing") {
+                state = "draw";
+        }
+        }
+
+        evaluateState();
+    }
+
+
+
+    const playRound = (e) => {
+        if(Game.board[e.target.id] === null){
+            Game.add(turn.type, e.target.id)
+            View.update(e.target.id)
+            checkResult(e)
+        } else {
+            changeTurn();
+        }
 
     }
 
 
-    document.querySelector(".gameboard").addEventListener("click", (e) => {
-        let currPlayer = turn.play(e.target.id);
-        checkLegal(Gameboard.gameboard, currPlayer.position)
-        Gameboard.add(currPlayer)
-        
-    
+    document.querySelector(".gameboard").addEventListener("click", (e)=>{
+        playRound(e)
+        changeTurn()
+    }) 
 
-        displayController.reset()
-        displayController.updateBoard(Gameboard.gameboard)
-        changeTurn();
-        checkState();
+
+    document.getElementById("resetgameboard").addEventListener("click", (e) => {
+        resetAll();
 
     })
 
+
+
 })();
 
+
+
+
+
+
+//model
+    //Gameboard
+    //Player
+//view
+    //displayController
+
+//controller
